04623bb357cf5c035ca524a691c4135692dc9941
carla_floricel <carla.floricel@gmail.com>
2021-06-04 11:04:28 -0400

    broken homepage into smaller compoenents + styling

---
+++ .history/src/homepage.component_20210604100210.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"></h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"></h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"></h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"></h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"></h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
