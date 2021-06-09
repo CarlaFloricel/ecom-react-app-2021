b3ca15684c1399fe945be0266bdf438e6a685ac9
carla_floricel <carla.floricel@gmail.com>
2021-06-09 09:30:56 -0400

    solved signin/signout issue

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,49 +1,49 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAd4_2zC35MaC3SeTIRPS4TSIosekwznSc",
-    authDomain: "crwn-db-ff46b.firebaseapp.com",
-    projectId: "crwn-db-ff46b",
-    storageBucket: "crwn-db-ff46b.appspot.com",
-    messagingSenderId: "215188291634",
-    appId: "1:215188291634:web:4bf179a15eba242df83c2b",
-    measurementId: "G-GG2NTFY8SH"
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-    
-    const userRef = firestore.doc(`users/${userAuth.uid}`)
-    const snapShot = await userRef.get()
+  if (!userAuth) return;
 
-    if(!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date()
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            })
-        } catch(error){
-            console.log('error creating user', error.message)
-        }
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
-    return userRef
-}
+  }
 
-firebase.initializeApp(config)
+  return userRef;
+};
 
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider()
-provider.setCustomParameters({ prompt: 'select_account' })
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export default firebase
+export default firebase;
